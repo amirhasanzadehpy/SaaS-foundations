8aad257fd4dd86c673878dfae677df9beda5fe31
amirhasanzadehpy <amirhasanzadehpy@gmail.com>
2024-08-16 01:57:22 +0330

    add price to subscription and stripe

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import Group,Permission
 from django.conf import settings
 from django.db.models.signals import post_save
+from helpers.billing import create_product,create_price
 
 User = settings.AUTH_USER_MODEL
+
 ALLOW_CUSTOM_GROUP = True
 
 SUBSCRIPTIONS_PERMISSIONS = [
@@ -13,6 +15,10 @@ SUBSCRIPTIONS_PERMISSIONS = [
         ]
 
 class Subscriptions(models.Model):
+
+    """
+    subscription plan = stripe product
+    """
     name = models.CharField(max_length=120)
     groups = models.ManyToManyField(Group)
     active = models.BooleanField(default=True)
@@ -20,11 +26,71 @@ class Subscriptions(models.Model):
         "content_type__app_label": "subscriptions",
         "codename__in": [x[0]for x in SUBSCRIPTIONS_PERMISSIONS]
     })
+    stripe_id = models.CharField(max_length=120, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
     class Meta:
         permissions = SUBSCRIPTIONS_PERMISSIONS
+
+    def save(self,*args, **kwargs):
+        if not self.stripe_id:
+                stripe_id = create_product(name=self.name,metadata={
+                    "subscription_plan_id": self.id
+                }, raw=False)
+
+                self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+
+
+class SubscriptionPrice(models.Model):
+    """
+    Subscription Price = Stripe Price
+    """
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = "month", "Monthly"
+        YEARLY = "year", "Yearly"
+
+    subscription = models.ForeignKey(Subscriptions, on_delete=models.SET_NULL, null=True)
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
+    interval = models.CharField(max_length=120, 
+                                default=IntervalChoices.MONTHLY, 
+                                choices=IntervalChoices.choices
+                            )
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+
+    @property
+    def stripe_currency(self):
+        return "usd"
+    
+    @property
+    def stripe_price(self):
+        """
+        remove decimal places
+        """
+        return int(self.price * 100)
+
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+    
+    def save(self, *args, **kwargs):
+        if (not self.stripe_id and 
+            self.product_stripe_id is not None):
+            stripe_id = create_price(
+                currency=self.stripe_currency,
+                unit_amount=self.stripe_price,
+                interval=self.interval,
+                product=self.product_stripe_id,
+                metadata={
+                        "subscription_plan_price_id": self.id
+                },
+                raw=False
+            )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
 
 
 class UserSubscription(models.Model):
